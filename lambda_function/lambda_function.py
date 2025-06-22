b9fac7148da64e1771602f06c9e9b8861cd09b82
svadwlas <sha.vadwalas@gmail.com>
2025-06-22 16:56:04 -0400

    Add files via upload

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -1,1 +1,20 @@
+import boto3
+import csv
+import os
 
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE']
+    table = dynamodb.Table(table_name)
+
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    response = s3.get_object(Bucket=bucket, Key=key)
+    lines = response['Body'].read().decode('utf-8').splitlines()
+
+    reader = csv.DictReader(lines)
+    for row in reader:
+        table.put_item(Item=row)
+
+    return {'statusCode': 200, 'body': 'CSV processed successfully'}
