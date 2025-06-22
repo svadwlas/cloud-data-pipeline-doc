a1907fa39f5046d13afb5397c3230715b4525bbf
svadwlas <sha.vadwalas@gmail.com>
2025-06-22 16:56:58 -0400

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "cloud-data-pipeline-poc-bucket"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "data_table" {
+  name           = "CloudDataPipelineTable"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
